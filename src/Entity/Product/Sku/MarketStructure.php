addf4b5e28affe3103cd2ebe82da0c4c36906d9b
Arthur Guimarães <arthurprogramming@gmail.com>
2018-05-21 19:41:33 -0300

    Fix

---
+++ src/Entity/Product/Sku/MarketStructure.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of gpupo/submarino-sdk
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gpupo\SubmarinoSdk\Entity\Product\Sku;
+
+use Gpupo\CommonSdk\Entity\EntityAbstract;
+use Gpupo\CommonSdk\Entity\EntityInterface;
+
+class MarketStructure extends EntityAbstract implements EntityInterface
+{
+    public function getSchema()
+    {
+        return [
+            'categoryId'    => 'integer',
+            'subCategoryId' => 'integer',
+        ];
+    }
+}
